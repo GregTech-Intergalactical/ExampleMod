f66a9fba2a17b3dfbc671e4042933cf8ae8c2b2c
Albert <albertsjolund@hotmail.com>
2021-11-27 20:13:34 +0100

    update examplemod

--- src/main/java/com/example/examplemod/ExampleData.java
+++ src/main/java/com/example/examplemod/ExampleData.java
@@ -1,5 +1,6 @@
 package com.example.examplemod;
 
+import muramasa.antimatter.AntimatterAPI;
 import muramasa.antimatter.item.ItemBasic;
 import muramasa.antimatter.material.Element;
 import muramasa.antimatter.material.Material;
@@ -9,7 +10,7 @@ public class ExampleData {
 
     public static TextureSet SET_DULL = new TextureSet(ExampleMod.ID, "dull");
 
-    public static Material ALUMINIUM = new Material(ExampleMod.ID, "aluminium", 0x80c8f0, SET_DULL, Element.Al).asMetal(933, 1700);
+    public static Material ALUMINIUM = AntimatterAPI.register(Material.class, new Material(ExampleMod.ID, "aluminium", 0x80c8f0, SET_DULL, Element.Al).asMetal(933, 1700));
 
     public static ItemBasic<?> IRIDIUM_ALLOY_INGOT = new ItemBasic<>(ExampleMod.ID, "iridium_alloy_ingot");
 
--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,10 +1,13 @@
 package com.example.examplemod;
 
 import muramasa.antimatter.AntimatterAPI;
+import muramasa.antimatter.AntimatterDynamics;
 import muramasa.antimatter.datagen.providers.AntimatterBlockStateProvider;
 import muramasa.antimatter.datagen.providers.AntimatterItemModelProvider;
+import muramasa.antimatter.event.AntimatterProvidersEvent;
 import muramasa.antimatter.registration.RegistrationEvent;
 import muramasa.antimatter.AntimatterMod;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,16 +20,21 @@ public class ExampleMod extends AntimatterMod {
     public static final String ID = "examplemod", NAME = "Example Mod";
 
     public ExampleMod() {
-        AntimatterAPI.addProvider(ID, g -> new AntimatterBlockStateProvider(ID, NAME + " BlockStates", g));
-        AntimatterAPI.addProvider(ID, g -> new AntimatterItemModelProvider(ID, NAME + " Item Models", g));
+        AntimatterDynamics.addProvider(ID, g -> new AntimatterBlockStateProvider(ID, NAME + " BlockStates", g));
+        AntimatterDynamics.addProvider(ID, g -> new AntimatterItemModelProvider(ID, NAME + " Item Models", g));
     }
-
+    
     @Override
-    public void onRegistrationEvent(RegistrationEvent event) {
+    public void onRegistrationEvent(RegistrationEvent event, Dist side) {
         switch (event) {
             case DATA_INIT:
                 ExampleData.init();
                 break;
-        }
+        }        
+    }
+
+    @Override
+    public String getId() {
+        return "example";
     }
 }
